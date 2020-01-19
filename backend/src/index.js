a975589932e8f052b658f3a8ebd24a70ca28c025
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2020-01-19 08:31:59 -0300

    Cors configuration added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
